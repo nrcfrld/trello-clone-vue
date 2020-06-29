cb8aa468843570275ab504eb6a8768f0421d7e53
nrcfrld <nrcfrld@gmail.com>
2020-06-29 08:41:48 +0700

    first commit

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+import store from './store'
+
+export function uuid() {
+  return Math.random()
+    .toString(16)
+    .slice(2)
+}
+
+export function saveStatePlugin(store) {
+  store.subscribe((mutation, state) =>
+    localStorage.setItem('boards', JSON.stringify(state.boards))
+  )
+}
